eebdf5c4b897c8c9e07779d1c087e327cc3e6447
karamkharma <karamkharma6@gmail.com>
2025-08-18 23:11:16 +0300

    first commit2s

---
+++ migrate.py
@@ -0,0 +1,11 @@
+import asyncio
+from alembic import command
+from alembic.config import Config
+
+def run_migrations():
+    # نربط Alembic بملف الإعدادات
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+if __name__ == "__main__":
+    run_migrations()
